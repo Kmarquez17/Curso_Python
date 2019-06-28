7f1f748db9a4d1c3fadc50c5b77a4c6412d4006b
kmarquez <kmarquez@thedataage.com>
2019-06-28 16:58:27 -0600

    Manejo de archivos con python

--- platzi-ventas/main.py
+++ platzi-ventas/main.py
@@ -1,19 +1,32 @@
 import sys
+import csv
+import os
+
+CLIENT_TABLE = '.clients.csv'
+CLIENT_SCHEMA = ['name', 'company', 'email', 'position']
 
 clients = [
-    {
-        'name': 'Pablo',
-        'company': 'Google',
-        'email': 'pablo@google.com',
-        'position': 'Sofware Engineer',
-    },
-    {
-        'name': 'Kevin',
-        'company': 'Facebook',
-        'email': 'Kmarquez@facebook.com',
-        'position': 'Frontend Developer',
-    },
+
 ]
+
+
+def _initialize_client_from_storage():
+    with open(CLIENT_TABLE, mode='r') as f:
+        reader = csv.DictReader(f, fieldnames=CLIENT_SCHEMA)
+
+        for row in reader:
+            clients.append(row)
+
+
+def _save_clients_to_storage():
+    tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+    with open(tmp_table_name, mode='w') as f:
+        writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
+        writer.writerows(clients)
+
+        os.remove(CLIENT_TABLE)
+        f.close()
+        os.rename(tmp_table_name, CLIENT_TABLE)
 
 
 def _get_client_from_user():
@@ -117,6 +130,7 @@ def _print_welcome():
 
 
 if __name__ == '__main__':
+    _initialize_client_from_storage()
     _print_welcome()
     command = input()
     command = command.upper()
@@ -124,22 +138,18 @@ if __name__ == '__main__':
     if command == 'C' or command == 'c':
         client = _get_client_from_user()
         create_clients(client)
-        list_clients()
 
     elif command == 'L':
         list_clients()
-
     elif command == 'U':
-        list_clients()
+
         client_id = int(_get_client_field('id'))
         updated_client = _get_client_from_user()
         update_client(client_id, updated_client)
-        list_clients()
 
     elif command == 'D':
         client_id = int(_get_client_field('id'))
         delete_client(client_id)
-        list_clients()
 
     elif command == 'S':
         client_name = _get_client_field('name')
@@ -152,6 +162,7 @@ if __name__ == '__main__':
     else:
         print('Invalid Command')
 
+    _save_clients_to_storage()
     # list_clients()
     # create_clients('Kevin')
     # list_clients()
